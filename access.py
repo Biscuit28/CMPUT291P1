31e5c6b06f8cf5ea0103ef3e7121e548aa84aec1
machine 3 <shangchen@machines-MacBook-Pro.local>
2017-10-30 17:57:32 -0600

    added get products

--- access.py
+++ access.py
@@ -1,5 +1,5 @@
 import sqlite3
-from collections import default_dict
+from collections import defaultdict
 
 
 class access:
@@ -37,7 +37,6 @@ class access:
 
         self.cursor.execute("SELECT * FROM customers WHERE name=:usr;",{"usr": username})
         res=self.cursor.fetchall()
-
         if len(res) > 0:
             print "Username already exists"
             return (False, "account already exists")
@@ -51,6 +50,7 @@ class access:
             print "Account created!"
             return (True, "Account created!")
 
+
     def login(self, username, password, customer=True):
 
         '''
@@ -58,7 +58,6 @@ class access:
         success, returns false otherwise
         '''
         username = username.lower()
-
         if customer:
             self.cursor.execute("SELECT * FROM customers WHERE name=:usr AND pwd=:pwd;", {"usr": username, "pwd": password})
         else:
@@ -75,11 +74,9 @@ class access:
         '''
         Querys items based on keywords, assumes keywords is a list
         '''
-        products = default_dict(int)
-
+        products = defaultdict(int)
         for word in keywords:
-
-            SQL = "SELECT * FROM products WHERE name LIKE %{}%;".format(word)
+            SQL = "SELECT * FROM products WHERE name LIKE '%{}%';".format(word)
             self.cursor.execute(SQL)
             results=self.cursor.fetchall()
             for result in results:
@@ -87,11 +84,81 @@ class access:
                     products[result[0]] = 1
                 else:
                     products[result[0]] += 1
+        return sorted(products, key=products.get, reverse=True)
 
-        sorted(d, key=d.get, reverse=True)
-        print products
+
+    def get_products(self, products):
+        '''
+        Takes in a list of products, and finds the details according to the
+        following specification:
+
+        For each matching product, list the product id, name, unit, the number
+        of stores that carry it, the number of stores that have it in stock, the
+        minimum price among the stores that carry it, the minimum price among the
+        stores that have the product in stock, and the number of orders within the past 7 days.
+        '''
+        #initialize condition
+        cond = ""
+        cond2 = ""
+        for prd in products:
+            cond += " prd.pid='{}' OR".format(prd)
+            cond2 += " oln.pid='{}' OR".format(prd)
+
+        #query 1 - product id, name, unit
+        q1 = "SELECT prd.pid, prd.name, prd.unit FROM products prd \
+        WHERE"+cond[:-2]
+
+        #query 2 - number of stores that carry it
+        q2 = "SELECT prd.pid, COUNT(crr.sid) FROM products prd, carries crr ON prd.pid=crr.pid \
+        WHERE"+cond[:-2]+" GROUP BY prd.pid"
+
+        #query 3 - the number of stores that have it in stock
+        q3 = "SELECT prd.pid, COUNT(crr.sid) FROM products prd, carries crr ON prd.pid=crr.pid \
+        WHERE"+cond[:-2]+" AND crr.qty > 0 GROUP BY prd.pid"
+
+        #query 4 - the minimum price among the stores that carry it
+        q4 = "SELECT prd.pid, crr.uprice FROM products prd, carries crr ON prd.pid=crr.pid \
+        WHERE" +cond[:-2] + " AND crr.uprice = (SELECT MIN(crr2.uprice) FROM carries crr2 WHERE crr.pid=crr2.pid)"
+
+        #query 5 - the number of orders within the past 7 days
+        q5 = "SELECT prd.pid, crr.uprice FROM products prd, carries crr ON prd.pid=crr.pid \
+        WHERE" +cond[:-2] + " AND crr.uprice = (SELECT MIN(crr2.uprice) FROM carries crr2 WHERE crr.pid=crr2.pid AND crr.qty > 0)"
+
+        #query 6 - the number of orders within the past 7 days ??
+        q6 = "SELECT oln.pid, COUNT(oln.oid) FROM orders ord, olines oln ON ord.oid=oln.oid \
+        WHERE ord.odate BETWEEN DATETIME('now') AND DATETIME('now', '-7 day') AND" +cond2[:-2]+" GROUP BY oln.pid"
+        #print q6
+        #main
+        main = "SELECT * FROM ((((({}) LEFT OUTER JOIN ({}) USING (pid)) LEFT OUTER JOIN ({}) \
+        USING (pid)) LEFT OUTER JOIN ({}) USING (pid)) LEFT OUTER JOIN ({}) USING (pid)) \
+        LEFT OUTER JOIN ({}) USING (pid);".format(q1, q2, q3, q4, q5, q6)
+
+        self.cursor.execute(main)
+        return self.cursor.fetchall()
+
+
+    def product_details(self, product_id):
+        '''
+        Takes in a product ID and returns the details of that product. the
+        details returned should adhere to the following specification
+
+        product id, name, unit, category and a listing of all stores that carry
+        the product with their prices, quantities in stock and the number of orders
+        within the past 7 days. If a product is carried by more than one store,
+        the result should be ordered as follows:
+
+        (1) the stores that have the product in stock will be listed before those that don't;
+
+        (2) the stores in each case will be sorted based on the store price (from lowest to highest).
+        '''
+
+        pass
+
+
 
 if __name__ == "__main__":
     a = access()
     #a.create_account("bobbylee", "4567311", "45673111", "2005 Hilliard Place NW")
     #a.login("bobbylee", "45673111", customer=False)
+    r= a.search(["canned", "beef"])
+    a.get_products(r)
